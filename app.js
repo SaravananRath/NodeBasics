9db3b44fafaeb477a9b83ecdd2d33eb2f2491d57
SaravananRath <saravananr95m16@gmail.com>
2018-07-19 17:22:26 +0530

    Http Server

--- app.js
+++ app.js
@@ -14,10 +14,10 @@
 const Logger = require('./logger')
 const logger = new Logger()
 
-logger.on('EventFired',(arg)=>{
-    console.log('EventListened',arg)
-})
-logger.log('Module With listener')
+// logger.on('EventFired',(arg)=>{
+//     console.log('EventListened',arg)
+// })
+// logger.log('Module With listener')
 
 const path = require('path')
 const pathObj = path.parse(__filename)
@@ -47,3 +47,23 @@ const fs = require('fs');
 // emitter.emit('EventFired')
 // emitter.on('EventFired',function(arg){
 
+const http = require('http')
+// const server = http.createServer()
+// server.on('connection',(socket)=>{
+//     console.log('New Connection')
+// })
+// server.listen(3000);
+// console.log('Listening on port 3000')
+const server = http.createServer((req,res)=>{
+    if(req.url==='/'){
+        res.write('Hello World')
+        res.end()
+    }
+    if(req.url==='/api/numbers'){
+        res.write(JSON.stringify([1,2,3]))
+        res.end()
+    }
+})
+
+server.listen(3000)
+console.log('Listening on port 3000')
